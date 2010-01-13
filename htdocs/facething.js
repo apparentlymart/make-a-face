9332a82e757df4da4a8ba4ed31cbb7e661e977b4
Martin Atkins <mart@degeneration.co.uk>
2010-01-12 20:53:50 -0800

    Use CSS background-size to make the images from typepad exactly fit our boxes.
    This allows the use of a larger image size that allows the enlarged versions to scale nicely.

--- htdocs/facething.js
+++ htdocs/facething.js
@@ -4,9 +4,11 @@ function onCellCreate(cell) {
     cell.elem.css("background-position", "center");
     cell.elem.css("background-repeat", "no-repeat");
     cell.elem.css("background-image", "url(http://static.typepad.com/.shared/images/default-avatars/avatar-03-250x250.gif)");
+    cell.elem.css("background-size", "100%");
+    cell.elem.css("-moz-background-size", "100%");
 
     if (cell.x == 4 && cell.y == 4) {
-        cell.elem.css("background-image", "url(http://up4.typepad.com/6a010535617444970b0120a5aa75ce970c-150si)");
+        cell.elem.css("background-image", "url(http://up4.typepad.com/6a010535617444970b0120a5aa75ce970c-250si)");
         cell.elem.css("-webkit-box-shadow", "0 0 20px #000000");
         cell.elem.css("-webkit-transform", "Scale(1.5)");
         cell.elem.css("-moz-box-shadow", "0 0 20px #000000");
@@ -15,10 +17,10 @@ function onCellCreate(cell) {
         //cell.elem.css("outline", "2px solid white");
     }
     else if (cell.x == 6 && cell.y == 2) {
-        cell.elem.css("background-image", "url(http://up3.typepad.com/6a00d83451ce6b69e200e5500958118834-150si)");
+        cell.elem.css("background-image", "url(http://up3.typepad.com/6a00d83451ce6b69e200e5500958118834-250si)");
     }
     else if (cell.x == 1 && cell.y == 3) {
-        cell.elem.css("background-image", "url(http://up3.typepad.com/6a00e54feb2fc38833010535dc4b63970b-150si)");
+        cell.elem.css("background-image", "url(http://up3.typepad.com/6a00e54feb2fc38833010535dc4b63970b-250si)");
     }
     else if (cell.x == 0 && cell.y == 0) {
         cell.elem.css("background-image", "url(uglylogo.png)");
