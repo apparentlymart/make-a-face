68b219a500bbebd6cd2e52d536cea5933a17311b
Martin Atkins <mart@degeneration.co.uk>
2010-01-13 14:55:21 -0800

    Load the flash thing into the right spot.

--- htdocs/facething.js
+++ htdocs/facething.js
@@ -1,4 +1,6 @@
 
+var createdCamera = false;
+
 function onCellCreate(cell) {
     cell.elem.css("background-color", "#aaaaff");
     cell.elem.css("background-position", "center");
@@ -7,7 +9,34 @@ function onCellCreate(cell) {
     cell.elem.css("background-size", "100%");
     cell.elem.css("-moz-background-size", "100%");
 
-    if (cell.x == 2 && cell.y == 1) {
+    if (cell.x == -1 && cell.y == 1) {
+        // This is where the flash thing goes.
+        cell.elem.css("background-image", "url(http://static.typepad.com/.shared/images/default-avatars/avatar-01-250x250.gif)");
+        cameraman.url = 'http://cameramanexample.appspot.com/static/cameraman/';
+        var cameraContainer = $('<div id="camera"></div>');
+        cell.elem.append(cameraContainer);
+        var cameraSize = spanSize(3);
+
+        window.setTimeout(function () {
+                // Disabled for now
+                createdCamera = true;
+                if (! createdCamera) {
+                    var mycam = cameraman.createCamera({
+                            'id': 'camera',
+                            'width': cameraSize,
+                            'height': cameraSize,
+                            'sendto': 'http://cameramanexample.appspot.com/upload',
+                            'errorSending': function(cam, err) { errorSending(cam, err) },
+                            'tookPhoto': function(cam) { tookPhoto(cam) },
+                            'droppedPhoto': function(cam) { droppedPhoto(cam) },
+                            'sentPhoto': function(cam, url) { sentPhoto(cam, url) }
+                        });
+                    createdCamera = true;
+                }
+            }, 2000);
+
+    }
+    else if (cell.x == 2 && cell.y == 1) {
         cell.elem.css("background-image", "url(http://up4.typepad.com/6a010535617444970b0120a5aa75ce970c-250si)");
         cell.elem.css("-webkit-box-shadow", "0 0 20px #000000");
         cell.elem.css("-webkit-transform", "Scale(1.1)");
@@ -35,8 +64,7 @@ addCreateListener(onCellCreate);
 $(document).ready(
     function () {
         initializeGrid();
-
-        cells[0][-3].setBothspan(2);
+        cells[1][-1].setBothspan(3);
     }
 );
 
